a90c48948ce7341a02cceb04498c81b73823aa52
DSalado97 <danielsalado@protonmail.com>
2023-02-09 11:39:58 +0100

    [ Componente ] Creado componente de login-user

--- venus-app/src/app/app.module.ts
+++ venus-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginUserComponent } from './login-user/login-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginUserComponent
   ],
   imports: [
     BrowserModule,
