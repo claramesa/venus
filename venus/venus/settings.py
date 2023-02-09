9609694c5795b214f68444ef01dc7a6f9ec21ac5
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-02-09 12:33:05 +0100

    [ Auth ] Creacion de la a clase Auth que se encargara de devolver y crear el token

--- venus/venus/settings.py
+++ venus/venus/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'django_expiring_token',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,3 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuracion del token, the default value is 1 day
+EXPIRING_TOKEN_DURATION=timedelta(hours=168)
+
+# Configuracion de seguridad del token, para que tenga la funcion de expirar
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'django_expiring_token.authentication.ExpiringTokenAuthentication',
+    ),
+}
--- venus/venus/urls.py
+++ venus/venus/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from .auth import Auth
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', Auth.as_view())
 ]
