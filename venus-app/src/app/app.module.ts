1d8997f6f17f54537a4db21a247309205c68a2d1
DSalado97 <danielsalado@protonmail.com>
2023-02-10 09:00:27 +0100

    [ Librerias ] Se han añadido las siguientes librerias: animate, sweetalert, bootstrap, local-storage. [ Componente ] Se ha creado el componente login-user y se han creado la interfaz de Usuario y su clase. [ Codigo ] En api-service.ts se ha escrito código para la busqueda del usuario, no está terminado.

--- venus-app/src/app/app.module.ts
+++ venus-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginUserComponent } from './login-user/login-user.component';
+import { ApiServiceProvider } from './provider/api-service/api-service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { LoginUserComponent } from './login-user/login-user.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ApiServiceProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
