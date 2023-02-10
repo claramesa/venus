92904f308709c1be0e7114d96d170716c85293c2
DSalado97 <danielsalado@protonmail.com>
2023-02-10 10:21:43 +0100

    [Servicio, Componentes ] hemos tocada el routing y creado la api

--- venus-app/src/app/app-routing.module.ts
+++ venus-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginUserComponent } from './login-user/login-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginUserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- venus-app/src/app/app.module.ts
+++ venus-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +15,12 @@ import { ApiServiceProvider } from './provider/api-service/api-service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot([
+     
+      {path: 'login', component: LoginUserComponent},
+      {path: '', redirectTo: '/login', pathMatch: 'full'},
+    ]),
   ],
   providers: [ApiServiceProvider],
   bootstrap: [AppComponent]
