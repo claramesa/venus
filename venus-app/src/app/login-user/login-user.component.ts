5ab69dde0ba0cc9915948c526b72577f50118dee
jose-moro2daw <jose.moro.luque.alu@iesjulioverne.es>
2023-02-13 08:18:35 +0100

    [Login, Api] Actualización del metodo login

--- venus-app/src/app/login-user/login-user.component.ts
+++ venus-app/src/app/login-user/login-user.component.ts
@@ -3,32 +3,32 @@ import { Observable } from 'rxjs';
 import { Usuario } from '../modelo/Usuario';
 import { ApiServiceProvider } from '../provider/api-service/api-service';
 import Swal from "sweetalert2"
-import { FormGroup, FormControl,FormBuilder,Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login-user',
   templateUrl: './login-user.component.html',
   styleUrls: ['./login-user.component.css']
 })
 export class LoginUserComponent implements OnInit {
-  
+
   //formulario
   profileForm = this.fb.group({
     username: ['', Validators.required],
     password: ['', Validators.required],
   });
   //Guarda los errores
-  errores:String[]=[]
+  errores: String[] = []
   //Si es exito  muestra que es exito
-  exito:boolean=false;
-  constructor(public api: ApiServiceProvider,private fb: FormBuilder) {
-    this.errores=new Array();
+  exito: boolean = false;
+  constructor(public api: ApiServiceProvider, private fb: FormBuilder) {
+    this.errores = new Array();
   }
 
   //Consulta el usuario a la api y comprueba si es correcto
   consultar(user: Usuario) {
     this.api.getUsuario(user).then((res) => {
       if (res.user) {
-        this.exito=true;
+        this.exito = true;
         Swal.fire({
           title: 'Correcto',
           text: `Buenas ${user.username}`,
@@ -39,10 +39,10 @@ export class LoginUserComponent implements OnInit {
         })
       }
 
-    }).catch((error)=>{
+    }).catch((error) => {
       console.log(error);
-      if(!error.user){
-        let meensajeError:string=error.message;
+      if (!error.user) {
+        let meensajeError: string = error.message;
         this.errores.push(meensajeError);
         Swal.fire({
           title: 'Error',
@@ -51,46 +51,55 @@ export class LoginUserComponent implements OnInit {
           imageWidth: 400,
           imageHeight: 200,
           imageAlt: 'Custom image',
-        })    
+        })
       }
     })
 
   }
 
   //Cambio de cada input controlando el evento 
-  onChangeInput(ev:Event){
-     this.errores=[];
+  onChangeInput(ev: Event) {
+    this.errores = [];
   }
 
   //Metodo de envio del formulario
-  onSubmit(){
-    let user:Usuario= new Usuario(0,"","")
-    this.errores=[];
-    this.exito=false;
-    user.username=this.profileForm.value.username ? this.profileForm.value.username:"" ;
-    user.password=this.profileForm.value.password ? this.profileForm.value.password:"" ;
+  onSubmit() {
+    let user: Usuario = new Usuario(0, "", "")
+    this.errores = [];
+    this.exito = false;
+    user.username = this.profileForm.value.username ? this.profileForm.value.username : "";
+    user.password = this.profileForm.value.password ? this.profileForm.value.password : "";
     this.consultar(user);
   }
 
- /* emailDomainValidator(control: FormControl) { 
-    let email = control.value; 
-    if (email && email.indexOf("@") != -1) { 
-      let [_, domain] = email.split("@"); 
-      if (domain !== "codecraft.tv") { 
-        return {
-          emailDomain: {
-            parsedDomain: domain
-          }
-        }
-      }
-    }
-    return null; 
-  }*/
+  /* emailDomainValidator(control: FormControl) { 
+     let email = control.value; 
+     if (email && email.indexOf("@") != -1) { 
+       let [_, domain] = email.split("@"); 
+       if (domain !== "codecraft.tv") { 
+         return {
+           emailDomain: {
+             parsedDomain: domain
+           }
+         }
+       }
+     }
+     return null; 
+   }*/
 
 
+  login() {
+    let user: Usuario = new Usuario(0, "", "")
+    user.username = this.profileForm.value.username ? this.profileForm.value.username : "";
+    user.password = this.profileForm.value.password ? this.profileForm.value.password : "";
+    this.api.checkUser(user).subscribe((data) => {
+      console.log(data);
+    })
+
+  }
 
   ngOnInit(): void {
-   this.api.getAllUser().subscribe((param) => {
+    this.api.getAllUser().subscribe((param) => {
       //console.log(param);
 
     })
--- venus-app/src/app/provider/api-service/api-service.ts
+++ venus-app/src/app/provider/api-service/api-service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Usuario } from 'src/app/modelo/Usuario';
 import { InterfaceUser } from 'src/app/modelo/interfaces';
 import { set, remove } from "local-storage"
@@ -10,26 +10,16 @@ import { map } from 'rxjs/operators';
 export class ApiServiceProvider {
 
     private URL = "https://63e5f85a83c0e85a868a1df1.mockapi.io/api/who/users";
-    constructor(public http: HttpClient) { }
+    private URLDjango = "https:/localhost:8000";
+
+    constructor(public http: HttpClient, public header: HttpHeaders) { }
 
     authenticatedusuario() {
         return localStorage.getItem("user");
-
     }
-    /*
-    Este método devuelve un objeto 'Promise'.
-    Esto es un elemento asíncrono que puede finalizar de dos formas: correctamente, en cuyo caso sale con resolve, o bien de forma incorrecta, acabando en ese caso con reject.
-    El método llama al método get del atributo http, pasándole como parámetro la url que devuelve la colección alumnos de la Api.
-    Lo que devuelve este método lo convertimos a Promise, para luego evaluar su resultado mediante then y catch.
-    Si el acceso a la Api ha ido bien el código que se ejecuta es el ubicado en la cláusula then. Si ha ido mal se ejecuta el código ubicado en la cláusula catch.
-    Si todo ha ido bien convertimos el array de objetos Json que nos llega a un array de objetos alumnos, y lo devolvemos con resolve.
-    Si el acceso ha ido mal devolvemos el mensaje de error que no llega mediante reject.
-    */
 
     getAllUser(): Observable<[]> {
-
         return this.http.get<any>(`${this.URL}`);
-
     }
 
     getUsuario(user: Usuario) {
@@ -38,19 +28,14 @@ export class ApiServiceProvider {
             this.http.get(this.URL).toPromise()
                 .then((data: any) => {
                     usuarioVar = data.map((res: Usuario) => {
-                        if (res.username === user.username){
-                            
-                            if(res.password === user.password) {
-                                localStorage.setItem("user_token",JSON.stringify("__TOKEN__"));
-                                resolve({"user":true,"message":"Usuario correcto"});
+                        if (res.username === user.username) {
+                            if (res.password === user.password) {
+                                localStorage.setItem("user_token", JSON.stringify("__TOKEN__"));
+                                resolve({ "user": true, "message": "Usuario correcto" });
                             }
-                           
-                            reject({"user":false,"message":"Contraseña incorrecta"});
-
+                            reject({ "user": false, "message": "Contraseña incorrecta" });
                         }
-                        reject({"user":false,"message":"No existe el usuario"});
-
-                      
+                        reject({ "user": false, "message": "No existe el usuario" });
                     });
 
                 })
@@ -60,6 +45,17 @@ export class ApiServiceProvider {
         });
 
         return promise;
+    }
+
+    checkUser(user: Usuario) {
+
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        const body = {
+            'username': user.username,
+            'password': user.password
+        }
+        return this.http.post(this.URLDjango + "/api/auth", body, { headers });
+
     }
 
 }//end_class
