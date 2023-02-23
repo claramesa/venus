d0b173afd7dad569f15023e761509ac385ae96a9
DSalado97 <danielsalado@protonmail.com>
2023-02-23 09:08:55 +0100

    [Interfaz] Interfaz bonita.

--- venus-app/src/app/app-routing.module.ts
+++ venus-app/src/app/app-routing.module.ts
@@ -4,31 +4,10 @@ import { FootbarComponent } from './footbar/footbar.component';
 import { HomeVenusComponent } from './home-venus/home-venus.component';
 import { LoginUserComponent } from './login-user/login-user.component';
 
-<<<<<<< HEAD
 var routes= [
   { path: '', component: HomeVenusComponent },
   { path: 'login', component: LoginUserComponent }
 ];
-=======
-//Recoge el campo de storage
-const local =localStorage.getItem("user_token") ? true:false;
-var routes: Routes = [];
-//Si esta activo cambia el route
-if(local){
-   routes= [
-    { path: '', component: FootbarComponent },
-    
-
-  ];
-}else{
-  console.log("Entada")
-  routes= [
-    { path: '', component: FootbarComponent },
-    { path: 'login', component: LoginUserComponent }
-  ];
-  
-}
->>>>>>> 6fa0902ae8c1b0cc77a586726012953309074cc4
 
 
 @NgModule({
