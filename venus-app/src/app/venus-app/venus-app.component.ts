6c7933248c7eb751ddd1e1c264f8c7a5c185b9c9
gjescuderoamodeo <gjescuderoamodeo@gmail.com>
2023-02-23 07:57:13 +0100

    [venus-app]
    Añadido estilos al card item, añadido enrutamiento al pulsar en un item del card

--- venus-app/src/app/venus-app/venus-app.component.ts
+++ venus-app/src/app/venus-app/venus-app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-venus-app',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./venus-app.component.css']
 })
 export class VenusAppComponent {
+
+  //lista contiene objetos (imagen, nombre, ruta(lugar de la app a acceder))
+  listaAplicaciones=[]
+
+  constructor(private router: Router) {}
+
+  navigateToVenus() {
+    this.router.navigate(['/']);
+  }
+
 
 }
