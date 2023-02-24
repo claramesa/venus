423049c4bdc4beb98e5eca1bea44e64f6eaaadcb
miguelangel <miguel.angel.garrido.gavilan.alu@iesjulioverne.es>
2023-02-24 08:33:46 +0100

    [ Inventario ] Añadidas las rutas del componente inventario al módulo de la aplicación

--- venus-app/src/app/app-routing.module.ts
+++ venus-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InventarioComponent } from './inventario/inventario.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'inventario',
+  title: 'Inventario',
+  component: InventarioComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- venus-app/src/app/app.module.ts
+++ venus-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventarioComponent } from './inventario/inventario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventarioComponent
   ],
   imports: [
     BrowserModule,
